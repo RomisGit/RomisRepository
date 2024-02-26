367fa7bc8bd443eda45ff7e16720370f7508bcf9
romit <romit@192.168.0.212>
2024-02-26 22:30:18 +0000

    Access Modifier

---
+++ MyMavenProject/src/main/java/AccessModifier/AccessPublic.java
@@ -0,0 +1,13 @@
+package AccessModifier;
+
+public class AccessPublic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PublicAccess obj=new PublicAccess();
+		System.out.println(obj.a);
+		obj.show();
+
+	}
+
+}
---
+++ MyMavenProject/src/main/java/AccessModifier/PublicAccess.java
@@ -0,0 +1,10 @@
+package AccessModifier;
+
+public class PublicAccess {
+	public int a=5;
+	public void show()
+	{
+		System.out.println("Hey");
+	}
+
+}
